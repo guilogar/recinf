0612997961c9451ef22cad173878d0b4753ade57
guilogar <jdkdejava@gmail.com>
2019-03-21 09:32:12 +0100

    Ejercicios de la practica 1 hechos.

--- practicas/1/Veinte.java
+++ practicas/1/Veinte.java
@@ -1,6 +1,36 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+
 public class Veinte {
     
-    public static void main (String[] args) {
+    public static void main (String[] args) throws Exception
+    {
         
+        File file = new File("EjercicioExpresiones.txt");
+        BufferedReader br = new BufferedReader(new FileReader(file));
+        String texto = "", st;
+        
+        while ((st = br.readLine()) != null) texto += st;
+        
+        String[] simbolos = {":", ",", "\\.", ";", "\\?", "¿", "¡", "!", "\\.\\.\\.", "\"", "'", "<<", ">>"};
+        
+        for (int i = 0; i < simbolos.length; i++)
+        {
+            texto = texto.replaceAll(simbolos[i], "");
+        }
+        
+        String[] vocalesAcentuadas = {"á", "é", "í", "ó", "ú"};
+        String[] vocalesNoAcentuadas = {"a", "e", "i", "o", "u"};
+        
+        for (int i = 0; i < vocalesAcentuadas.length; i++)
+        {
+            texto = texto.replaceAll(vocalesAcentuadas[i], vocalesNoAcentuadas[i]);
+        }
+        
+        texto = texto.replaceAll(" \\d+ ", " ");
+        texto = texto.toUpperCase();
+        texto = texto.replaceAll("  ", " ");
+        System.out.println(texto);
     }
 }
